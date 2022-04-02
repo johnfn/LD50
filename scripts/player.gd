257248346316ecec0734eb19ab7a0554c8b472db
johnfn <johnfn@gmail.com>
2022-04-02 03:06:54 -0400

    some basic movement stuff

---
+++ scripts/player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+var size = 64
+var tick = 0
+
+var ticks_to_move = 0.25
+
+func round_position():
+  position.x = round(position.x / size) * size
+  position.y = round(position.y / size) * size
+
+func _ready():
+  round_position()
+
+func _process(delta):
+  var dist = Vector2.ZERO
+  
+  tick += delta
+  
+  if Input.is_action_pressed("ui_left") and dist == Vector2.ZERO:
+    dist.x -= size
+  
+  if Input.is_action_pressed("ui_right") and dist == Vector2.ZERO:
+    dist.x += size
+
+  if Input.is_action_pressed("ui_up") and dist == Vector2.ZERO:
+    dist.y -= size
+  
+  if Input.is_action_pressed("ui_down") and dist == Vector2.ZERO:
+    dist.y += size
+  
+  var can_move = tick >= ticks_to_move
+  
+  if can_move and dist != Vector2.ZERO:
+    move_and_collide(dist)
+    round_position()
+    tick = 0.0
+  
+  print(position)
+  
+  for i in get_slide_count():
+      var collision = get_slide_collision(i)
+      print(collision)
+
