e43b823b540c2cef36ec25e161c1adb5e2171016
Mike Precup <arch@minipepper.games>
2022-04-02 19:40:53 -0400

    Added physics layers

--- scripts/shadow_spawner.gd
+++ scripts/shadow_spawner.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var Shadow = preload("res://Shadow.tscn")
 
+export var grid_offset : Vector2 = Vector2.ZERO
+export var grid_size : float = 64
+
+var boundary_shadows = []
+
 func _ready():
   var shadow = Shadow.instance()
   
@@ -17,3 +22,11 @@ func _process(delta):
   var results = space.intersect_point(
     Vector2(32, 32), 32, [], 2147483647, true
   )
+
+func _physics_process(delta):
+  var to_remove = []
+  for i in range(len(boundary_shadows)):
+    var shadow = boundary_shadows[i]
+  
+  for i in range(len(to_remove)):
+    boundary_shadows.remove(to_remove[len(to_remove)-1-i])
