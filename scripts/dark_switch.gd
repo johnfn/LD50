92ae275f04e42d4fbafc6906be5535eaf1938d7d
Mike Precup <arch@minipepper.games>
2022-04-02 22:56:29 -0400

    Light switch added

--- scripts/dark_switch.gd
+++ scripts/dark_switch.gd
@@ -5,6 +5,7 @@ export var linked_doors : Array
 
 # These are Doors
 var linked_door_nodes : Array
+var has_been_triggered : bool = false
 
 func _ready():
   for door_path in linked_doors:
@@ -12,6 +13,7 @@ func _ready():
 
 func _on_Area_body_entered(body):
   # This is hopefully only shadows
-  if body != Globals.Player:
+  if body != Globals.Player and not has_been_triggered:
+    has_been_triggered = true
     for door in linked_door_nodes:
       door.toggle_open()
--- scripts/light_switch.gd
+++ scripts/light_switch.gd
@@ -3,12 +3,23 @@ extends Node2D
 # These are NodePaths
 export var linked_doors : Array
 
+export var player_light_distance : float = 1000
+export var torch_light_distance : float = 1000
+export var raycast_mask : int = 0b1011100
+
 # These are Doors
 var linked_door_nodes : Array
+var is_on : bool = false
 
 func _ready():
   for door_path in linked_doors:
     linked_door_nodes.append(get_node(door_path))
 
 func _physics_process(delta):
-  pass
+  if Globals.Player.position.distance_to(position) < player_light_distance:
+    var space = get_world_2d().get_direct_space_state()
+    var raycast_result = space.intersect_ray(position, Globals.Player.position, [], raycast_mask)
+    if (len(raycast_result) > 0) == is_on:
+      is_on = not is_on
+      for door in linked_door_nodes:
+        door.toggle_open()
