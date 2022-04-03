bc1e15e2fa240fac0ae82dc2a18f35cefb1ba335
Mike Precup <arch@minipepper.games>
2022-04-02 20:18:57 -0400

    Shadows propagate

--- scripts/shadow_spawner.gd
+++ scripts/shadow_spawner.gd
@@ -4,29 +4,55 @@ var Shadow = preload("res://Shadow.tscn")
 
 export var grid_offset : Vector2 = Vector2.ZERO
 export var grid_size : float = 64
+export var shadow_propogation_time : float = 1
+# TODO should this include the player?
+export var raycast_mask = 0b11100
+export var movable_raycast_mask = 0b1000000
 
 var boundary_shadows = []
+var spawn_offsets = [
+    Vector2(0, grid_size),
+    Vector2(0, -grid_size),
+    Vector2(grid_size, 0),
+    Vector2(-grid_size, 0),
+  ]
 
 func _ready():
+  spawn_shadow(grid_offset)
+  
+func spawn_shadow(spawn_position: Vector2):
   var shadow = Shadow.instance()
-  
-  
-  shadow.position.x = 128
-  shadow.position.y = 128
-
+  shadow.position = spawn_position
+  shadow.modulate = Color(1, 1, 1, 0)
   add_child(shadow)
-  
+  boundary_shadows.append(shadow)
 
-func _process(delta):
-  var space = get_world_2d().get_direct_space_state()
-  var results = space.intersect_point(
-    Vector2(32, 32), 32, [], 2147483647, true
-  )
 
 func _physics_process(delta):
+  var space = get_world_2d().get_direct_space_state()
   var to_remove = []
   for i in range(len(boundary_shadows)):
-    var shadow = boundary_shadows[i]
+    var shadow : Shadow = boundary_shadows[i]
+    if shadow.time_since_spawn > shadow_propogation_time:
+      var shadow_center = shadow.position + Vector2(grid_size / 2, grid_size / 2)
+      shadow.modulate = Color(1, 1, 1, 1)
+      var blocked = 0
+      for spawn_offset in spawn_offsets:
+        var raycast_results = space.intersect_ray(shadow_center, shadow_center + spawn_offset, [shadow], raycast_mask)
+        if len(raycast_results) > 0:
+          blocked += 1
+        else:
+          raycast_results = space.intersect_ray(shadow_center, shadow_center + spawn_offset, [shadow], movable_raycast_mask)
+          if len(raycast_results) == 0:
+            spawn_shadow(shadow.position + spawn_offset)
+            blocked += 1
+      
+      if blocked == len(spawn_offsets):
+        to_remove.append(i)
+    else:
+      shadow.time_since_spawn += delta
+      shadow.modulate = Color(1, 1, 1, shadow.time_since_spawn / shadow_propogation_time)
   
   for i in range(len(to_remove)):
     boundary_shadows.remove(to_remove[len(to_remove)-1-i])
+    
