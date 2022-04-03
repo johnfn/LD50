621c79e85cf4f1a66b77b5b76933b2501be5f722
Mike Precup <arch@minipepper.games>
2022-04-02 21:33:30 -0400

    Refactor doors and switches in preperation for more types

---
+++ scripts/Door.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+class_name Door
+
+export var is_door_open = false
+
+func toggle_open():
+  if is_door_open:
+    is_door_open = false
+    get_node("StaticBody/CollisionShape").set_deferred("disabled", false)
+    get_node("LightOccluder2D").visible = true
+    get_node("Sprite").modulate.a = 1.0
+  else:
+    is_door_open = true
+    get_node("StaticBody/CollisionShape").set_deferred("disabled", true)
+    get_node("LightOccluder2D").visible = false
+    get_node("Sprite").modulate.a = 0.5  
--- scripts/switch.gd
+++ scripts/switch.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
-signal player_enter()
+# These are NodePaths
+export var linked_doors : Array
+
+# These are Doors
+var linked_door_nodes : Array
+
+func _ready():
+  for door_path in linked_doors:
+    linked_door_nodes.append(get_node(door_path))
 
 func _on_Area_body_entered(body):
   if body == Globals.Player:
-    emit_signal("player_enter")
+    for door in linked_door_nodes:
+      door.toggle_open()
