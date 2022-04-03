f3e82453370150c8e23f155bc71b4949669ac480
johnfn <johnfn@gmail.com>
2022-04-03 19:03:25 -0400

    add switch gfx

--- scripts/proximity_switch.gd
+++ scripts/proximity_switch.gd
@@ -9,6 +9,8 @@ var linked_door_nodes : Array
 onready var og_is_on = is_on
 
 func _ready():
+  update_gfx()
+  
   for door_path in linked_doors:
     linked_door_nodes.append(get_node(door_path))
 
@@ -17,6 +19,11 @@ func _on_Area_body_entered(body):
     is_on = not is_on
     for door in linked_door_nodes:
       door.toggle_open()
+  update_gfx()
+  
+func update_gfx():
+  $Sprite.visible = not is_on
+  $SpritePressed.visible = is_on
 
 func reset():
   is_on = og_is_on
