83fafe92c80daa0ee1ca015487af7ad3d88f6088
johnfn <johnfn@gmail.com>
2022-04-04 15:24:06 -0400

    prob not worth it

--- scripts/torch.gd
+++ scripts/torch.gd
@@ -1,7 +1,24 @@
 extends Node2D
 
+var interactor = preload("res://Interactor.tscn")
+
 func reset():
   queue_free()
+
+func _ready():
+  initially_hide_interactor()
+
+# hide interactor for a sec so that it doesnt seem spammy when u immediately
+# drop lantern
+func initially_hide_interactor():
+  var pos = $Interactor.position
+  $Interactor.queue_free()
+  
+  yield(get_tree().create_timer(1.0), "timeout")
+  
+  var i = interactor.instance()
+  add_child(i)
+  i.position = pos
 
 func _unhandled_input(event):
   if Input.is_action_just_pressed("interact"):
