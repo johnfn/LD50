21245bf730112e656287261ec0668b06622bb972
johnfn <johnfn@gmail.com>
2022-04-03 20:31:38 -0400

    dumb stuff

--- scripts/wall.gd
+++ scripts/wall.gd
@@ -20,6 +20,27 @@ export(Facing) var facing = Facing.North
 
 func _ready():
   set_wall_vis()
+  
+  if not Engine.editor_hint:
+    show_proper_light_occluder()
+
+func show_proper_light_occluder():
+  if facing == Facing.North:
+    $Graphics/WallN/LightOccluder2D.queue_free()
+  if facing == Facing.East:
+    $Graphics/WallE/LightOccluder2D.queue_free()
+  if facing == Facing.South:
+    $Graphics/WallS/LightOccluder2D.queue_free()
+  if facing == Facing.West:
+    $Graphics/WallW/LightOccluder2D.queue_free()
+  if facing == Facing.EastTall:
+    $Graphics/WallETall/LightOccluder2D.queue_free()
+  if facing == Facing.WestTall:
+    $Graphics/WallWTall/LightOccluder2D.queue_free()
+  if facing == Facing.SouthWestTall:
+    $Graphics/WallSWTall/LightOccluder2D.queue_free()
+  if facing == Facing.SouthEastTall:
+    $Graphics/WallSETall/LightOccluder2D.queue_free()
 
 func set_wall_vis():
   for child in $Graphics.get_children():
@@ -33,16 +54,12 @@ func set_wall_vis():
     $Graphics/WallS.visible = true
   if facing == Facing.West:
     $Graphics/WallW.visible = true
-    
   if facing == Facing.EastTall:
     $Graphics/WallETall.visible = true
-    
   if facing == Facing.WestTall:
     $Graphics/WallWTall.visible = true
-    
   if facing == Facing.SouthWestTall:
     $Graphics/WallSWTall.visible = true
-  
   if facing == Facing.SouthEastTall:
     $Graphics/WallSETall.visible = true
 
