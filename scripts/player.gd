e866b482a48b3edf1523a0262772a1f52185616e
Mike Precup <arch@minipepper.games>
2022-04-04 12:36:12 -0400

    death/stairs/respawn anims

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,11 @@ var size = Globals.grid_size
 var tick = 0
 onready var shadow_checker = get_node("/root/Root/ShadowChecker")
 
+export var screen_wipe_time = 1
+var wipe_time = 0
+var enclose_time = 0
+var is_dead = false
+
 # var ticks_to_move = 0.25
 var ticks_to_move = 0.25
 onready var dialog = $Dialog
@@ -20,6 +25,7 @@ func respawn(spawn_point: Vector2):
   shadow_checker.update_flood_fill_based_on_player_location()
   if Globals.current_level == 1:
     trigger_level_start_shadows()
+  is_dead = false
 
 func round_position(target):
   target.position.x = round(target.position.x / size) * size
@@ -76,7 +82,8 @@ func _unhandled_input(event):
       new_torch.global_position = global_position
     
   if Input.is_action_just_pressed("respawn"):
-    StateManager.return_to_checkpoint()
+    wipe_time = 0.001
+    is_dead = true
   
   var new_press_order = []
   for action_name in press_order:
@@ -120,6 +127,31 @@ func set_facing(dir):
 
 func _physics_process(delta):
   if Globals.game_mode() != "normal":
+    return
+  
+  if wipe_time > 0:
+    wipe_time += delta
+  if enclose_time > 0:
+    enclose_time += delta
+  var wipe_perc = wipe_time / screen_wipe_time
+  var enclose_perc = enclose_time / screen_wipe_time
+  if wipe_perc > 0.5 and wipe_perc - delta / screen_wipe_time <= 0.5:
+    if is_dead:
+      StateManager.return_to_checkpoint()
+    else:
+      move_to_level_start()
+  if enclose_perc > 0.5 and enclose_perc - delta / screen_wipe_time <= 0.5:
+    if is_dead:
+      StateManager.return_to_checkpoint()
+  var overlay = $CanvasLayer/Overlay
+  overlay.material.set_shader_param("wipe_percent", wipe_perc)
+  overlay.material.set_shader_param("enclose_percent", enclose_perc)
+  if wipe_time > screen_wipe_time:
+    wipe_time = 0
+  if enclose_time > screen_wipe_time:
+    enclose_time = 0
+  
+  if is_dead or wipe_time > 0:
     return
   
   tick += delta
@@ -198,12 +230,11 @@ func start_dying_co():
   
   print("YOU HAVE DIED")
   
-  # TODO: Death cinematic
-  
-  StateManager.return_to_checkpoint()
+  enclose_time = 0.001
+  is_dead = true
   
 
 func enter_stairs(var from_level):
   Globals.current_level = from_level + 1
   
-  move_to_level_start()
+  wipe_time = 0.001
